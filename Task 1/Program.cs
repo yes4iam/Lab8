8b520dc0f8924b99476abb9e022b64e55200c62b
Ilya Markov <imarkovkin@gmail.com>
2022-11-21 01:26:23 +0300

    Добавьте файлы проекта.

---
+++ Task 1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\imark\Desktop\Автоматизация проектирования\Lesson 8\Lab 8\Lab 8\Temp";
+            string[] files = Directory.GetFiles(path, "*.*", SearchOption.AllDirectories);
+            foreach (string file in files)
+            {
+                Console.WriteLine(file);
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Task 2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\imark\Desktop\Автоматизация проектирования\Lesson 8\Lab 8\Lab 8\Temp\Task 2.txt";
+            Random random = new Random();
+            int sum = 0;
+            using (StreamWriter sw = new StreamWriter(path, false))
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    sw.WriteLine(random.Next(-20, 20));
+                }
+                                                       
+            }
+            using (StreamReader sr = new StreamReader(path))
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    sum += Convert.ToInt32(sr.ReadLine());
+                }
+                              
+            }
+            Console.WriteLine(sum);
+            Console.ReadKey();
+        }
+    }
+}
